710e80470f61a98df6ea3ca1da6ac21c1bb2fdff
Siddhesh <51325660+pocketleague@users.noreply.github.com>
2019-12-09 18:44:53 +0530

    no message

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public int id;
+	public string name;
+	
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
