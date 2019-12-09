eb7e736cf198013fa1fe30ffd3b85538b1a7bce1
Harry7895 <58207843+Harry7895@users.noreply.github.com>
2019-12-09 18:48:52 +0530

    no message

---
+++ Assets/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+	
+	 bool isgameOver;
+	 bool isWin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
